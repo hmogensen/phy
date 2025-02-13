55e2b19b913eaa984d71ba8a88d69c5f5f763fd7
unknown <hpmogen@gmail.com>
2025-02-13 12:18:37 +0100

    Add option to backup all data files

--- plugins/sc_settings_menu.py
+++ plugins/sc_settings_menu.py
@@ -2,7 +2,11 @@ import sys
 import subprocess
 import os
 import gc
+import shutil
+from pathlib import Path
 import numpy as np
+from PyQt5.QtWidgets import QFileDialog
+
 from phy import IPlugin, connect
 from phylib.io.sc_params_dialog import ScParamsDialog
 from phylib.io.sc_params import ScParams
@@ -12,6 +16,29 @@ def _edit_parameters(params:ScParams):
     if accepted:
         return updated_params
     return None
+
+def select_or_create_folder():
+    folder_path = QFileDialog.getExistingDirectory(
+        None,
+        caption="Select Folder",
+        directory=""
+    )
+    
+    if folder_path:
+        os.makedirs(folder_path, exist_ok=True)
+        return folder_path
+    
+    return None
+
+def copy_files(source_dir, dest_dir):
+    Path(dest_dir).mkdir(parents=True, exist_ok=True)    
+    patterns = ['*.npy', '*.toml', '*.toml.backup']
+    
+    for root, _, files in os.walk(source_dir):
+        for pattern in patterns:
+            for filepath in Path(source_dir).glob(pattern):
+                dest_path = os.path.join(dest_dir, filepath.name)
+                shutil.copy2(filepath, dest_path)
 
 # Close all data files so the won't be locked from access by other applications
 def close_all_open_files():
@@ -26,24 +53,24 @@ class ScSettingsMenuPlugin(IPlugin):
     def attach_to_controller(self, controller):
         @connect
         def on_gui_ready(sender, gui):
-            # TODO: Move to edit actions sub menu
             gui.file_actions.separator()
-            @gui.file_actions.add()
+            @gui.file_actions.add(toolbar=True) # Use icon 'f1de'
             def sc_settings():
                 params = controller.sc_params
                 if updated_params := _edit_parameters(params):
                     for key in updated_params:
                         params[key] = updated_params[key]
-            # @gui.file_actions.add()
-            # def backup_sc_params():
-            #     print("Save toml file to new place / with new name")
-            # @gui.file_actions.add()
-            # def save_project_to_new_directory():
-            #     print("save files to new directory")
-            #     print("restart phy in new directory")
-            @gui.file_actions.add()
+
+            @gui.file_actions.add(toolbar=True)
+            def backup_project():
+                script_dir = os.path.dirname(controller.sc_params.main.script_path)
+                abs_folder_path = script_dir + "/" + controller.sc_params.output_dir
+                new_folder_path = select_or_create_folder()
+                if new_folder_path:
+                    copy_files(abs_folder_path, new_folder_path)
+
+            @gui.file_actions.add(toolbar=True)
             def run_sc_sort():
-                print("Choose menu with selection (new output dir, etc)")
                 close_all_open_files()
 
                 start_point = "spike-detection"
